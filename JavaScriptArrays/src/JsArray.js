6519af1867adc77908d200b3da1d90dd530364ba
kosten00 <ylkahu@gmail.com>
2020-04-02 07:29:52 +0700

    JS arrays task updated.

--- JavaScriptArrays/src/JsArray.js
+++ JavaScriptArrays/src/JsArray.js
@@ -1,41 +1,46 @@
+(function ready() {
 // Создайте массив чисел
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 // Отсортируйте его по убыванию
-array = array.sort(function (e1, e2) {
-    return e2 - e1;
-});
-console.log(array);
+    array.sort(function (e1, e2) {
+        return e2 - e1;
+    });
+    console.log(array);
 
-// Получите подмассив из первых 5 элементов и
-// подмассив из последних 5 элементов
-let firstFiveElementsArray = array.slice(0, 5);
-let lastFiveElementsArray = array.slice(array.length - 5);
-console.log(firstFiveElementsArray);
-console.log(lastFiveElementsArray);
+// Получите подмассив из первых 5 элементов и подмассив из последних 5 элементов
+    var firstFiveElementsArray = array.slice(0, 5);
+    var lastFiveElementsArray = array.slice(array.length - 5);
 
-// Найдите сумму элементов массива, которые являются
-// четными числами
-let sum = 0;
-array.forEach(function (e) {
-    if (e % 2 === 0) {
-        sum+= e;
-    }
-});
-console.log(sum);
+    console.log(firstFiveElementsArray);
+    console.log(lastFiveElementsArray);
+
+// Найдите сумму элементов массива, которые являются четными числами
+    var evenSum = array
+        .filter(function (value) {
+            return value % 2 === 0 ? value : 0;
+        })
+        .reduce(function (sum, evenValue) {
+            return sum + evenValue;
+        });
+    console.log(evenSum);
 
 // Создайте массив чисел от 1 до 100, в таком порядке
-let start = 1;
-let arrayToFill = [];
-while (start <= 100) {
-    arrayToFill.push(start++);
-}
-console.log(arrayToFill);
+    var arrayToFill = [];
 
-//Получите список квадратов четных чисел из этого
-// массива
-arrayToFill.forEach(function (e) {
-    if (e % 2 === 0) {
-        console.log(e * e);
+    for (var i = 1; i <= 100; i++) {
+        arrayToFill.push(i);
     }
-});
+    console.log(arrayToFill);
+
+//Получите список квадратов четных чисел из этого массива
+    var newArray = arrayToFill
+        .filter(function (value) {
+            return value % 2 === 0;
+        })
+        .map(function (filteredValue) {
+            return filteredValue * filteredValue;
+        });
+
+    console.log(newArray);
+})();
