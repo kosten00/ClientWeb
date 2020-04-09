5ff871accdf138522abbabe8e7d044e698ba65be
kosten00 <ylkahu@gmail.com>
2020-04-10 00:35:34 +0700

    Js arrays task: fixed comments margins, fixed filter and reduce functions.

--- JavaScriptArrays/src/JsArray.js
+++ JavaScriptArrays/src/JsArray.js
@@ -1,31 +1,31 @@
 (function ready() {
-// Создайте массив чисел
+    // Создайте массив чисел
     var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-// Отсортируйте его по убыванию
+    // Отсортируйте его по убыванию
     array.sort(function (e1, e2) {
         return e2 - e1;
     });
     console.log(array);
 
-// Получите подмассив из первых 5 элементов и подмассив из последних 5 элементов
+    // Получите подмассив из первых 5 элементов и подмассив из последних 5 элементов
     var firstFiveElementsArray = array.slice(0, 5);
     var lastFiveElementsArray = array.slice(array.length - 5);
 
     console.log(firstFiveElementsArray);
     console.log(lastFiveElementsArray);
 
-// Найдите сумму элементов массива, которые являются четными числами
+    // Найдите сумму элементов массива, которые являются четными числами
     var evenSum = array
         .filter(function (value) {
-            return value % 2 === 0 ? value : 0;
+            return value % 2 === 0;
         })
         .reduce(function (sum, evenValue) {
             return sum + evenValue;
-        });
+        }, 0);
     console.log(evenSum);
 
-// Создайте массив чисел от 1 до 100, в таком порядке
+    // Создайте массив чисел от 1 до 100, в таком порядке
     var arrayToFill = [];
 
     for (var i = 1; i <= 100; i++) {
@@ -33,7 +33,7 @@
     }
     console.log(arrayToFill);
 
-//Получите список квадратов четных чисел из этого массива
+    //Получите список квадратов четных чисел из этого массива
     var newArray = arrayToFill
         .filter(function (value) {
             return value % 2 === 0;
