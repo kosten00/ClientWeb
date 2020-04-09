d1eeb4bad22839504509a67872ef825fbf9ab9a7
kosten00 <ylkahu@gmail.com>
2020-04-10 02:11:45 +0700

    JS object task: fixed old & added new functions, new html title.

--- JavaScriptObjects/src/JsObjects.js
+++ JavaScriptObjects/src/JsObjects.js
@@ -1,43 +1,68 @@
-let countries = [{
-    name: 'Russia', cities: [
-        {name: 'Novosibirsk', population: 1511000},
-        {name: 'Moscow', population: 11920000},
-        {name: 'Saint-Petersburg', population: 4991000}]
-}, {
-    name: 'England', cities: [
-        {name: 'London', population: 8900000},
-        {name: 'Manchester', population: 510746},
-        {name: 'Liverpool', population: 552267}]
-}, {
-    name: 'Canada', cities: [
-        {name: 'Toronto', population: 2930000},
-        {name: 'Montreal', population: 1780000},
-        {name: 'Vancouver', population: 675218},]
-}];
+(function ready() {
+    var countries = [{
+        name: 'Russia',
+        cities: [{
+            name: 'Novosibirsk',
+            population: 1511000
+        }, {
+            name: 'Moscow',
+            population: 11920000
+        }, {
+            name: 'Saint-Petersburg',
+            population: 4991000
+        }]
+    }, {
+        name: 'England',
+        cities: [{
+            name: 'London',
+            population: 8900000
+        }, {
+            name: 'Manchester',
+            population: 510746
+        }, {
+            name: 'Liverpool',
+            population: 552267
+        }]
+    }, {
+        name: 'Canada',
+        cities: [{
+            name: 'Toronto',
+            population: 2930000
+        }, {
+            name: 'Montreal',
+            population: 1780000
+        }, {
+            name: 'Vancouver',
+            population: 675218
+        }]
+    }];
 
-// Найдите страну/страны с максимальным количеством
-// городов
-let maxCitiesCount = 0;
+    // Найдите страну/страны с максимальным количеством городов
+    function getMaxCitiesCountCountries(countries) {
+        return countries
+            .filter(function (country) {
+                return country.cities.length === countries
+                    .reduce(function (sum, value) {
+                        return Math.max(value.cities.length, sum);
+                    }, 0);
+            });
+    }
 
-countries.forEach(function (e) {
-    let currentCitiesCount = e.cities.length;
-    maxCitiesCount = currentCitiesCount < maxCitiesCount ? maxCitiesCount : currentCitiesCount;
-});
+    console.log(getMaxCitiesCountCountries(countries));
 
-console.log(maxCitiesCount);
+    // Получите объект с информацией по всем странам такого вида: ключ – название страны, значение – суммарная численность по стране
+    function getCountriesInfo(countries) {
+        var countriesInfo = {};
 
-// Получите объект с информацией по всем странам такого
-// вида: ключ – название страны, значение – суммарная
-// численность по стране
-let countriesInfo = {};
+        countries.forEach(function (country) {
+            countriesInfo[country.name] = country.cities
+                .reduce(function (countryPopulation, city) {
+                    return countryPopulation + city.population;
+                }, 0);
+        });
 
-countries.forEach(function (e) {
-    let count = 0;
-    e.cities.forEach(function (city) {
-        count += city.population;
-    });
+        return countriesInfo;
+    }
 
-    countriesInfo[e.name] = count;
-});
-
-console.log(countriesInfo);
+    console.log(getCountriesInfo(countries));
+})();
