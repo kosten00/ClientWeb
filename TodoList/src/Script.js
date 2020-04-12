a6a59d530a91eb9e7fbb3dbf07d2722916ca59ce
kosten00 <ylkahu@gmail.com>
2020-04-12 12:01:39 +0700

    Todo list fixed, some code refactor.

--- TodoList/src/Script.js
+++ TodoList/src/Script.js
@@ -1,79 +1,92 @@
-(function ready() {
-    var inputField = document.querySelector(".input-text-field");
-    var addItemButton = document.querySelector(".add-item");
-    var listOfItems = document.querySelector(".list-of-items");
+document.addEventListener('DOMContentLoaded', function () {
+    var inputField = document.querySelector('.input-text-field');
+    var addItemButton = document.querySelector('#add-item');
+    var listOfItems = document.querySelector('.list-of-items');
 
-    addItemButton.addEventListener("click", function () {
-        var textFromTextfield = inputField.value;
-        var errorMessage = document.querySelector(".error-message");
+    addItemButton.addEventListener('click', function () {
+        var textFromTextField = inputField.value.trim();
+        var errorMessage = document.querySelector('.error-message');
 
-        if (textFromTextfield === "") {
-            errorMessage.style.color = "#ff2020";
-
+        if (textFromTextField === '') {
+            errorMessage.classList.add('error-message-show');
+            inputField.value = '';
             return;
         }
-        var listItem = document.createElement("li");
-        listItem.className = "items-in-list";
 
-        var stringItem = document.createElement("p");
-        stringItem.className = "list-item-text";
-        stringItem.textContent = textFromTextfield;
-        listItem.appendChild(stringItem);
+        var listItem = document.createElement('li');
+        listItem.className = 'items-in-list';
 
-        var editButton = document.createElement("button");
-        editButton.textContent = "edit";
+        var textItem = document.createElement('p');
+        textItem.className = 'list-item-text';
+        textItem.textContent = textFromTextField;
+        listItem.appendChild(textItem);
+
+        var editButton = document.createElement('button');
+        editButton.className = 'todo-form-button';
+        editButton.textContent = 'edit';
         listItem.appendChild(editButton);
 
-        var deleteButton = document.createElement("button");
-        deleteButton.textContent = "delete";
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'todo-form-button';
+        deleteButton.textContent = 'delete';
+
         listItem.appendChild(deleteButton);
 
         listOfItems.appendChild(listItem);
 
-        errorMessage.style.color = "#fffdd9";
-        inputField.value = "";
+        errorMessage.classList.remove('error-message-show');
+        inputField.value = '';
 
-        deleteButton.addEventListener("click", function () {
+        deleteButton.addEventListener('click', function () {
             listOfItems.removeChild(listItem);
         });
 
-        editButton.addEventListener("click", function () {
+        editButton.addEventListener('click', function () {
             var editField = document.createElement('input');
             editField.type = 'text';
-            editField.className = "edit-field";
+            editField.className = 'edit-field';
+            editField.value = textFromTextField;
 
-            stringItem.replaceWith(editField);
-            editField.value = textFromTextfield;
+            textItem.parentNode.replaceChild(editField, textItem);
 
-            var saveButton = document.createElement("button");
-            saveButton.textContent = "save";
-            editButton.replaceWith(saveButton);
+            var saveButton = document.createElement('button');
+            saveButton.className = 'todo-form-button';
+            saveButton.textContent = 'save';
 
-            var cancelButton = document.createElement("button");
-            cancelButton.textContent = "cancel";
-            deleteButton.replaceWith(cancelButton);
+            editButton.parentNode.replaceChild(saveButton, editButton);
 
-            saveButton.addEventListener("click", function () {
-                var newStringValue = editField.value;
+            var cancelButton = document.createElement('button');
+            cancelButton.className = 'todo-form-button';
+            cancelButton.textContent = 'cancel';
 
-                if (newStringValue === "") {
-                    errorMessage.style.color = "#ff2020";
+            deleteButton.parentNode.replaceChild(cancelButton, deleteButton);
+
+            saveButton.addEventListener('click', function () {
+                var newTextValue = editField.value.trim();
+
+                textFromTextField = newTextValue;
+
+                if (newTextValue === '') {
+                    errorMessage.classList.add('error-message-show');
+                    editField.value = '';
 
                     return;
                 }
-                editField.replaceWith(stringItem);
-                stringItem.textContent = newStringValue;
 
-                saveButton.replaceWith(editButton);
-                cancelButton.replaceWith(deleteButton);
+                errorMessage.classList.remove('error-message-show');
+
+                editField.parentNode.replaceChild(textItem, editField);
+                textItem.textContent = newTextValue;
+
+                saveButton.parentNode.replaceChild(editButton, saveButton);
+                cancelButton.parentNode.replaceChild(deleteButton, cancelButton);
             });
 
-            cancelButton.addEventListener("click", function () {
-                editField.replaceWith(stringItem);
-
-                saveButton.replaceWith(editButton);
-                cancelButton.replaceWith(deleteButton);
+            cancelButton.addEventListener('click', function () {
+                editField.parentNode.replaceChild(textItem, editField);
+                saveButton.parentNode.replaceChild(editButton, saveButton);
+                cancelButton.parentNode.replaceChild(deleteButton, cancelButton);
             });
         })
     });
-})();
+});
