c85086bbc031c38fa246d29fdf228d6d97f72a3c
Константин <ylkahu@gmail.com>
2020-04-28 22:40:37 +0700

    PhoneBook jquery css classes naming changed, styles changed, script variables naming changed, removed es6 syntax. Filter-block logic changed. Some code refactor.

--- PhonebookJQuery/src/Script.js
+++ PhonebookJQuery/src/Script.js
@@ -1,13 +1,16 @@
-jQuery(function () {
-    var surnameField = $('#surnameInput');
-    var nameField = $('#nameInput');
-    var phoneField = $('#phoneInput');
-    var selectAllCheckbox = $('#selectAllCheckbox');
-    var filterInputField = $('#filterField');
+$(function () {
+    var filterBlock = $('.filter');
+    filterBlock.hide();
 
-    var addNewItemButton = $('#addNewItemButton');
-    var applyFilterButton = $('#applyFilterButton');
-    var cancelFilterButton = $('#cancelFilterButton');
+    var surnameField = $('#surname-input');
+    var nameField = $('#name-input');
+    var phoneField = $('#phone-input');
+    var selectAllCheckbox = $('#select-all-checkbox');
+    var filterInputField = $('#filter-field');
+
+    var addNewItemButton = $('#add-new-item-button');
+    var applyFilterButton = $('#apply-filter-button');
+    var cancelFilterButton = $('#cancel-filter-button');
 
     var phones = [];
 
@@ -32,28 +35,35 @@ jQuery(function () {
             surnameField.parent().addClass('red-background');
             emptyFields.push('surname');
         }
+
         if (name === '') {
             nameField.parent().addClass('red-background');
             emptyFields.push('name');
         }
+
         if (phone === '') {
             phoneField.parent().addClass('red-background');
             emptyFields.push('phone');
         }
 
-        var incorrectInputInformer = $('#incorrectInputInformer');
+        var incorrectInputInformer = $('#incorrect-input-informer');
+
         if (emptyFields.length !== 0) {
             incorrectInputInformer.text('Fields ' + emptyFields.join(', ') + ' were not filled!').addClass('red-background');
 
             return;
         }
+
         incorrectInputInformer.text('Please input surname, name, phone number and press + button');
         $('.red-background').removeClass('red-background');
 
+        if (phones.indexOf(phone) === -1) {
+            phones.push(phone);
 
-        if (phones.indexOf(phone) !== -1) {
+            addEntry(itemIndex, surname, name, phone);
+        } else {
             $('<div></div>').appendTo('body')
-                .html('<div><h6>' + 'Do you want to add contact with phone number, that is already in phone-book?' + '?</h6></div>')
+                .html('<div><h6>Do you want to add contact with phone number, that is already in phone-book?</h6></div>')
                 .dialog({
                     modal: true,
                     title: 'Please validate',
@@ -64,26 +74,23 @@ jQuery(function () {
                     buttons: {
                         Yes: function () {
                             addEntry(itemIndex, surname, name, phone);
-                            $(this).dialog("close");
+                            $(this).dialog('close');
                         },
                         No: function () {
                             clearInputFields();
-                            $(this).dialog("close");
+                            $(this).dialog('close');
                         }
                     },
                     close: function () {
                         $(this).remove();
                     }
                 });
-        } else {
-            phones.push(phone);
-
-            addEntry(itemIndex, surname, name, phone);
         }
     });
 
     applyFilterButton.click(function () {
         var filterText = filterInputField.val().toLowerCase();
+
         if (filterText === '') {
             return;
         }
@@ -92,13 +99,12 @@ jQuery(function () {
         listItems.hide();
 
         listItems.each(function () {
-            if ($(this).children('.surname').text().toLowerCase().includes(filterText) ||
-                $(this).children('.name').text().toLowerCase().includes(filterText) ||
-                $(this).children('.phone').text().toLowerCase().includes(filterText)) {
-
+            if ($(this).children('.surname').text().toLowerCase().indexOf(filterText) !== -1 ||
+                $(this).children('.name').text().toLowerCase().indexOf(filterText) !== -1 ||
+                $(this).children('.phone').text().toLowerCase().indexOf(filterText) !== -1) {
                 $(this).show();
             }
-        })
+        });
     });
 
     cancelFilterButton.click(function () {
@@ -138,48 +144,50 @@ jQuery(function () {
                     .click(function () {
                         var buttonsToRemove = [];
 
-                        if (listItemRemoveButton.text() === 'remove all selected') {
+                        if (listItemRemoveButton.text() !== 'remove all selected') {
+                            buttonsToRemove.push(listItemRemoveButton);
+                        } else {
                             $('.list-item-checkbox:checked').each(function () {
                                 buttonsToRemove.push($(this).siblings('.remove-button'));
-                            })
-                        } else {
-                            buttonsToRemove.push(listItemRemoveButton);
+                            });
                         }
 
                         $('<div></div>').appendTo('body')
-                            .html('<div><h6>' + 'Remove item' + '?</h6></div>')
+                            .html('<div><h6>Remove item?</h6></div>')
                             .dialog({
                                 modal: true,
                                 title: 'Please validate',
                                 zIndex: 10000,
                                 autoOpen: true,
-                                width: 'auto',
+                                width: '300px',
                                 resizable: false,
                                 buttons: {
                                     Yes: function () {
-                                        buttonsToRemove.forEach(button => removeEntry(button, button.siblings('.phone').text()));
-                                        $(this).dialog("close");
+                                        buttonsToRemove.forEach(function (button) {
+                                            removeEntry(button, button.siblings('.phone').text());
+                                        })
+                                        $(this).dialog('close');
                                     },
                                     No: function () {
-                                        $(this).dialog("close");
+                                        $(this).dialog('close');
                                     }
                                 },
                                 close: function () {
                                     $(this).remove();
                                 }
-                            });
+                            }).css('font-size', '1.2em');
                     }))
         );
         clearInputFields();
 
-        changeFilterFieldPointerEvents();
+        changeFilterFieldStatus();
     }
 
-    function changeFilterFieldPointerEvents() {
+    function changeFilterFieldStatus() {
         if (getListLength() >= 2) {
-            $('.filter').css('pointer-events', 'auto');
+            filterBlock.show();
         } else {
-            $('.filter').css('pointer-events', 'none');
+            filterBlock.hide();
             selectAllCheckbox.prop('checked', false);
         }
     }
@@ -191,7 +199,7 @@ jQuery(function () {
 
         recountIndexes();
 
-        changeFilterFieldPointerEvents();
+        changeFilterFieldStatus();
     }
 
     function clearInputFields() {
@@ -205,10 +213,8 @@ jQuery(function () {
     }
 
     function recountIndexes() {
-        var counter = 1;
-
-        $('.index').each(function () {
-            $(this).text(counter++);
+        $('.index').each(function (index) {
+            $(this).text(index + 1);
         })
     }
 });
