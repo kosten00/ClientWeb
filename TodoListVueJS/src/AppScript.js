ff62f4bf0e66d328591e6bacb716e7ec28224342
Константин <ylkahu@gmail.com>
2020-05-07 18:17:12 +0700

    Fixed cancel button action from modal window by adding new close and cancel method. Removed margins from alert div. Added index from v-for directive. Removed todo id recount. Some naming refactoring.

--- TodoListVueJS/src/AppScript.js
+++ TodoListVueJS/src/AppScript.js
@@ -1,33 +1,13 @@
-// 1. Сейчас если в процессе редактирования заметки нажать Cancel, то сохраняется текущий текст заметки, а не тот, который был до начала редактирования
-//
-// 2. Сейчас элементы страницы не выровнены относительно друг друга.
-// Лучше выровнять их:
-// http://joxi.ru/nAyX3P8CjB6ak2
-// Аналогично в диалоге редактирования
-//
-// 3. id не должен использоваться в качестве номера задачи.
-// Для этой цели можно использовать index из v-for.
-// И id не должны пересчитываться, они должны задаваться 1 раз при создании заметки
-//
-// 7. В диалоге редактирования лучше сделать кнопкам разный цвет
-//
-// 9. Вместо такого:
-// @cancel="closeModal()"
-// Лучше делать так:
-// @cancel="closeModal"
-//
-// 10. currentTodo лучше изначально задать null вместо пустого объекта
-
 new Vue({
     el: '#app',
     data: {
-        id: 1,
+        id: 0,
         todoText: '',
         todos: [],
         isIncorrectInput: false,
         isRemoving: false,
         isEditing: false,
-        currentTodo: {},
+        currentTodo: null,
         editFieldText: '',
     },
     methods: {
@@ -41,21 +21,11 @@ new Vue({
             this.todoText = '';
         },
         remove: function () {
-            var currentTodoId = this.currentTodo.id;
             var todoToRemove = this.currentTodo;
 
             this.todos = this.todos.filter(function (todo) {
                 return todo !== todoToRemove;
             });
-
-            var filteredTodosLength = this.todos.length;
-            this.id = filteredTodosLength + 1;
-
-            if (currentTodoId !== filteredTodosLength + 1) {
-                for (var i = currentTodoId - 1; i < filteredTodosLength; i++) {
-                    this.todos[i].id = i + 1;
-                }
-            }
         },
         setCurrent: function (todo, event) {
             event.currentTarget.id === 'remove'
@@ -77,10 +47,10 @@ new Vue({
         isCorrectInput: function (inputText) {
             if (inputText.trim() === '') {
                 this.isIncorrectInput = true;
-                var that = this;
+                var self = this;
 
                 setTimeout(function () {
-                    that.isIncorrectInput = false;
+                    self.isIncorrectInput = false;
                 }, 2000);
 
                 return false;
@@ -90,14 +60,13 @@ new Vue({
 
             return true;
         },
-        closeModal: function () {
-            if (this.isEditing) {
-                this.currentTodo.text = this.editFieldText;
-                this.isEditing = false;
-                return;
-            }
-
-            this.isRemoving = false;
+        closeAndSave: function () {
+            this.currentTodo.text = this.editFieldText;
+            this.isEditing = false;
         },
+        closeAndCancel: function () {
+            this.isRemoving = false;
+            this.isEditing = false;
+        }
     }
 });
